3360c1b083e91fa019802c190ed8cd336fdb1eeb
rashikapr <74289013+rashikapr@users.noreply.github.com>
2023-03-09 11:32:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyADRJs2__X6avZtyn8nsK0EOLmv_L6fKYA",
+  authDomain: "wireless-buzzer-ef101.firebaseapp.com",
+  databaseURL: "https://wireless-buzzer-ef101-default-rtdb.firebaseio.com",
+  projectId: "wireless-buzzer-ef101",
+  storageBucket: "wireless-buzzer-ef101.appspot.com",
+  messagingSenderId: "61643369732",
+  appId: "1:61643369732:web:cc6085eaf1b1c4cdaeb451",
+  measurementId: "G-HDYN3G3E2W"
+};
+
